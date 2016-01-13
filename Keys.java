030a7b705d98d64918952b40832a5044b46e3138
Dorothy Carter <dorothy.carter5578@gmail.com>
2016-01-13 11:06:17 -0500

    Create Keys.java
    this is what the Keys class should look like, but with more API keys

---
+++ Keys.java
@@ -0,0 +1,11 @@
+// Dorothy Carter	Jan. 2016
+
+package twitter;
+
+public class Keys {
+  // you're still going to have to get your own keys
+	protected static final String consumerKey = "";
+	protected static final String consumerSecret = "";
+	protected static final String accessKey = "";
+	protected static final String accessSecret = "";
+}
