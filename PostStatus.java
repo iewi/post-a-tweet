9513da52d83c121d11a05faa6b0dc2c267cd045b
Dorothy Carter <dorothy.carter5578@gmail.com>
2016-02-04 16:58:18 -0500

    added oauth authentication bit

--- PostStatus.java
+++ PostStatus.java
@@ -3,7 +3,7 @@
 package twitter;
 
 import java.net.*;
-
+import java.util.Scanner;
 import java.io.*;
 
 import oauth.signpost.*;
@@ -13,17 +13,18 @@ import oauth.signpost.http.HttpParameters;
 public class PostStatus {
 	protected static URL goHere;
 	protected static OAuthConsumer consumer;
+	protected static OAuthProvider provider;
 	
 	public static void main(String[] args) throws IOException {
 		consumer = new DefaultOAuthConsumer(Keys.consumerKey, Keys.consumerSecret);
 		
-		consumer.setTokenWithSecret(Keys.accessKey, Keys.accessSecret);
+		getAccessToken();
 		
 		GenerateTweet tweet = new GenerateTweet();
 		
 		String params = "status=" + OAuth.percentEncode(tweet.status);
-		
 		HttpParameters encodedParams = new HttpParameters();
+		
 		encodedParams.put("status", OAuth.percentEncode(tweet.status));	
 		consumer.setAdditionalParameters(encodedParams);
 		
@@ -41,13 +42,30 @@ public class PostStatus {
 			os.write(params.getBytes());
 			os.flush(); os.close();
 			
-			cnx.connect();
+			//cnx.connect();
 			
 			if (cnx.getResponseCode() == 200)
 				System.out.println("Status posted");
 			else
 				System.err.println("Status not posted. Response code: " + cnx.getResponseCode());
 
+		} catch (Exception e) {e.printStackTrace(System.err);}
+	}
+	
+	public static void getAccessToken() {
+		Scanner scanPin = new Scanner(System.in);
+		provider = new DefaultOAuthProvider(
+                "https://api.twitter.com/oauth/request_token",
+                "https://api.twitter.com/oauth/access_token",
+                "https://api.twitter.com/oauth/authorize");
+		try {
+			String authUrl = provider.retrieveRequestToken(consumer, OAuth.OUT_OF_BAND);
+			
+			System.out.println("Visit:\n" + authUrl + "\n... and grant this app authorization");
+			System.out.println("Enter the PIN code and hit ENTER when you're done:");
+			String pin = scanPin.nextLine();
+			
+			provider.retrieveAccessToken(consumer, pin);
 		} catch (Exception e) {e.printStackTrace(System.err);}
 	}
 }
